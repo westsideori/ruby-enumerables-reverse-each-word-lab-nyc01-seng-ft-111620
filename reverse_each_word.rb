02503266f7fcdc789abe50f82fa39a5eeca98a9a
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 14:31:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   string_array = string.split(" ")
   reversed_string = ""
   string_array.each do |element|
-    reversed_string += element.reverse
+    reversed_string = element.reverse.join(" ")
   end
   reversed_string
 end
