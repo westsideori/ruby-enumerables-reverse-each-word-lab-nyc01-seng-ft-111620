0bc4837fe52e17429d335b9b8577e1c08d3ec1ff
Ori Markowitz <ori.markowitz@gmail.com>
2020-10-24 14:25:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-  string_array = string.split(",")
+  string_array = string.
   reversed_string = ""
   string_array.each do |element|
     reversed_string += element.reverse
